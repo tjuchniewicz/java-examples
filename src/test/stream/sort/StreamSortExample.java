0a1d12e1faaf38b72910268c38ecd2af822ff5b9
tjuchniewicz <tjuchniewicz@gmail.com>
2016-01-23 17:27:55 +0100

    Stream sorting example

--- src/test/stream/sort/StreamSortExample.java
+++ src/test/stream/sort/StreamSortExample.java
@@ -4,8 +4,48 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Example sorting using stream.
+ * 
+ * Shows how to sort by multiple fields which is not so easy to implement before Java 8
+ *
+ */
 public class StreamSortExample {
 
+	public static void main(String[] args) {
+
+		List<ObjectVO> objects = new ArrayList<>();
+		objects.add(new ObjectVO("John", 50));
+		objects.add(new ObjectVO("John", 60));
+		objects.add(new ObjectVO("Andy", 50));
+		objects.add(new ObjectVO("Andy", 55));
+		objects.add(new ObjectVO("Frank", 70));
+
+		System.out.println("(1) by name");
+		objects.stream().sorted(Comparator.comparing(ObjectVO::getName)).forEach(System.out::println);
+		System.out.println();
+
+		System.out.println("(2) by name reversed");
+		objects.stream().sorted(Comparator.comparing(ObjectVO::getName).reversed()).forEach(System.out::println);
+		System.out.println();
+		
+		System.out.println("(3) by name and then age");
+		objects.stream().sorted(Comparator.comparing(ObjectVO::getName).thenComparing(ObjectVO::getAge))
+				.forEach(System.out::println);
+		System.out.println();
+
+		System.out.println("(4) by name and then age reversed");
+		objects.stream().sorted(Comparator.comparing(ObjectVO::getName, Comparator.naturalOrder()).thenComparing(ObjectVO::getAge, Comparator.reverseOrder()))
+				.forEach(System.out::println);
+		System.out.println();
+		
+		System.out.println("(5) by (name and then age) and then all reversed");
+		objects.stream()
+				.sorted(Comparator.comparing(ObjectVO::getName).thenComparing(ObjectVO::getAge).reversed())
+				.forEach(System.out::println);
+		System.out.println();
+	}
+	
 	static class ObjectVO {
 		private String name;
 		private int age;
@@ -36,53 +76,5 @@ public class StreamSortExample {
 		public String toString() {
 			return "ObjectVO [name=" + name + ", age=" + age + "]";
 		}
-	}
-
-	public static void main(String[] args) {
-
-		List<ObjectVO> objects = new ArrayList<>();
-		objects.add(new ObjectVO("John", 50));
-		objects.add(new ObjectVO("John", 60));
-		objects.add(new ObjectVO("Andy", 50));
-		objects.add(new ObjectVO("Andy", 55));
-		objects.add(new ObjectVO("Frank", 70));
-
-		System.out.println("by name");
-		objects.stream().sorted(Comparator.comparing(ObjectVO::getName)).forEach(System.out::println);
-		System.out.println();
-
-		System.out.println("by name reversed");
-		objects.stream().sorted(Comparator.comparing(ObjectVO::getName).reversed()).forEach(System.out::println);
-		System.out.println();
-		
-		System.out.println("by name and age");
-		objects.stream().sorted(Comparator.comparing(ObjectVO::getName).thenComparing(ObjectVO::getAge))
-				.forEach(System.out::println);
-		System.out.println();
-
-		System.out.println("by name and age reversed");
-//		TODO why in this case we have:
-//			ObjectVO [name=John, age=60]
-//			ObjectVO [name=John, age=50]
-//			ObjectVO [name=Frank, age=70]
-//			ObjectVO [name=Andy, age=55]
-//			ObjectVO [name=Andy, age=50]
-//		?
-		objects.stream().sorted(Comparator.comparing(ObjectVO::getName).thenComparing(ObjectVO::getAge).reversed())
-				.forEach(System.out::println);
-		System.out.println();
-
-		System.out.println("by name reversed and age reversed");
-		objects.stream()
-				.sorted(Comparator.comparing(ObjectVO::getName).reversed().thenComparing(ObjectVO::getAge).reversed())
-				.forEach(System.out::println);
-		System.out.println();
-
-		System.out.println("by name reversed and age");
-		objects.stream()
-				.sorted(Comparator.comparing(ObjectVO::getName).reversed().thenComparing(ObjectVO::getAge))
-				.forEach(System.out::println);
-		System.out.println();
-		
 	}
 }
