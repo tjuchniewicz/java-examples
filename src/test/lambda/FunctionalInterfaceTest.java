0d3d5492573ebe29a4d71622fb3c5a21be51f469
Tomasz Juchniewicz <tjuchniewicz@gmail.com>
2016-01-31 15:04:39 +0100

    Default method example.

--- src/test/lambda/FunctionalInterfaceTest.java
+++ src/test/lambda/FunctionalInterfaceTest.java
@@ -12,10 +12,25 @@ public class FunctionalInterfaceTest {
 	}
 
 	@FunctionalInterface
-	// only one method allowed in interface
 	interface MyFunctionalParametrizedInterface {
 
 		String produce(Integer i);
+	}
+	
+	@FunctionalInterface
+	public interface CombinedInterface {
+		
+		String produce(Integer i);
+		
+		default CombinedInterface combine(CombinedInterface i) {
+			
+			return ((p) -> {
+				String s1 = produce(p);
+				String s2 = s1.concat("; ").concat(i.produce(p));
+				return s2;
+			});
+			
+		}
 	}
 
 	String myMethod(MySimpleFunctionalInterface f) {
@@ -27,7 +42,12 @@ public class FunctionalInterfaceTest {
 
 		return f.produce(10);
 	}
-
+	
+	String myCombinedMethod(CombinedInterface f) {
+		
+		return f.produce(25);
+	}
+	
 	@Test
 	public void passSimpleExpressionTest() {
 
@@ -38,5 +58,16 @@ public class FunctionalInterfaceTest {
 	public void passParametrizedExpressionTest() {
 
 		System.out.println(myParametrizedMethod(x -> "param test: " + x));
+	}
+	
+	@Test
+	public void combinedInterfaceTest() {
+		
+		CombinedInterface f1 = (x) -> "string 1: " + x;
+		CombinedInterface f2 = (x) -> "string 2: " + x;
+		CombinedInterface f3 = f1.combine(f2);
+		
+		System.out.println(myCombinedMethod(f3));
+		
 	}
 }
