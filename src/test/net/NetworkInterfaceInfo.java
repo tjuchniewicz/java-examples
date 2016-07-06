f567f477d4410ea8ba540c2a2e534716d9d680c3
Tomasz Juchniewicz <tjuchniewicz@gmail.com>
2016-07-06 19:07:04 +0200

    Add example how to read information about network interfaces

---
+++ src/test/net/NetworkInterfaceInfo.java
@@ -0,0 +1,30 @@
+package test.net;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class NetworkInterfaceInfo {
+
+	public static void main(String[] args) throws SocketException {
+		
+		for (Enumeration<NetworkInterface> nics = NetworkInterface
+				.getNetworkInterfaces(); nics.hasMoreElements();) {
+			NetworkInterface ifc = nics.nextElement();
+			
+			System.out.println("Found interface: " + ifc.getDisplayName());
+			
+			for (Enumeration<InetAddress> ias = ifc.getInetAddresses(); ias.hasMoreElements();) {
+				
+				InetAddress ia = ias.nextElement();
+				if (ia instanceof Inet4Address) {
+					System.out.println("   Found address: " + ia);	
+					System.out.println("      isSiteLocalAddress: " + ia.isSiteLocalAddress());	
+					System.out.println("      isLinkLocalAddress: " + ia.isLinkLocalAddress());	
+				}
+			}
+		}
+	}
+}
